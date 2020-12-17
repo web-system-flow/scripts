0472a9d9b190f414600491fda2f218015c4adac4
roselew <rozalewinska@gmail.com>
2020-12-17 23:39:10 +0100

    Add Cookie Script Without Data Sent

---
+++ cookie.js
@@ -0,0 +1,59 @@
+(()=> {
+    class Cookies {
+        constructor(elem) {
+            this.KEY_COOKIE = "SYSFLOW.COOKIE";
+
+            this.cookie = elem;
+
+            this.url = elem.dataset.sysflowCookie;
+
+            this.acceptBtn = this.cookie.querySelector("[data-sysflow-cookie-accept]");
+
+            this.cookieStatus = localStorage.getItem(this.KEY_COOKIE);
+
+            this.assignEvents();
+        }
+
+        assignEvents() {
+            if (!this.cookieStatus) {
+                this.appendCookie();
+            } else {
+                this.removeCookie();
+            }
+        }
+
+        appendCookie() {
+            setTimeout(() => {
+                this.cookie.classList.remove("invisible");
+            }, 100);
+            this.acceptBtn.addEventListener("click", this.acceptCookie.bind(this));
+        }
+
+        removeCookie(){
+            this.cookie.style.display = "none";
+        }
+
+        acceptCookie() {
+            localStorage.setItem(this.KEY_COOKIE, true);
+            this.removeCookie();
+            if(this.url!=="0" && this.url!=="false"){
+                this.sendWebhook();
+            } 
+        }
+
+        sendWebhook() {
+            const dataToSent = {
+                location: window.location.href
+            };
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("POST", this.url, true);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            xhttp.send(JSON.stringify(dataToSent));
+        }
+
+    }
+
+    const cookie = document.querySelector('[data-sysflow-cookie]');
+    new Cookies(cookie);
+
+})();
